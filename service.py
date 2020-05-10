14c537b1d231a5b4be12846b3f4b46e74b529052
saubhik <saubhik.mukherjee@gmail.com>
2020-05-10 20:23:47 +0900

    add ever growing list of useless arguments

--- service.py
+++ service.py
@@ -39,13 +39,21 @@ class Service:
         self.setup_configs()
 
         logging.info(msg="Starting a new session")
+        # https://bit.ly/2yz7W6T
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--disable-extensions")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--start-maximized")
         options.add_argument("--no-sandbox")
-
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-browser-side-navigation")
+        options.add_argument("--disable-gpu")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option(
+            "prefs", {"profile.managed_default_content_settings.images": 2}
+        )
         driver = webdriver.Chrome(options=options)
         driver.implicitly_wait(time_to_wait=5)
         driver.get(url="https://excise.wb.gov.in/eRetail/Page/eRetail_Home.aspx")
